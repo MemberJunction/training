e9d912ad5f58e07cac64e3dcc5815c8aceec9c94
jordanfanapour <jordan.fanapour@bluecypress.io>
2024-11-19 20:28:34 -0600

    Create setup script

---
+++ startup_script.ps1
@@ -0,0 +1,42 @@
+# Navigate to the script's directory
+cd $PSScriptRoot
+
+# CustomServerEntities
+Write-Output "Switching to the 'CustomServerEntities' directory..."
+cd .\CustomServerEntities\
+Write-Output "Linking 'GeneratedEntities' in 'CustomServerEntities'..."
+npm link "..\GeneratedEntities"
+Write-Output "Installing dependencies for 'CustomServerEntities'..."
+npm install
+Write-Output "Building 'CustomServerEntities'..."
+npm run build
+
+# Return to the root directory
+cd ..
+
+# MJAPI
+Write-Output "Switching to the 'MJAPI' directory..."
+cd .\MJAPI\
+Write-Output "Linking 'CustomServerEntities' in 'MJAPI'..."
+npm link "..\CustomServerEntities"
+Write-Output "Installing dependencies for 'MJAPI'..."
+npm install
+Write-Output "Building 'MJAPI'..."
+npm run build
+
+# Return to the root directory
+cd ..
+
+# Portal
+Write-Output "Switching to the 'abstracts/Portal' directory..."
+cd .\abstracts\Portal\
+Write-Output "Linking 'GeneratedActions' and 'GeneratedEntities' in 'Portal'..."
+npm link "..\..\GeneratedActions" "..\..\GeneratedEntities"
+Write-Output "Installing dependencies for 'Portal'..."
+npm install
+Write-Output "Building 'Portal'..."
+npm run build
+
+# Back to the root directory
+cd ../..
+Write-Output "All steps completed successfully!"
